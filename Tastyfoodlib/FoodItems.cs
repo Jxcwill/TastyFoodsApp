f02a74f284a2470b3d98579232ddf17979b3603a
Jxcwill <62357914+Jxcwill@users.noreply.github.com>
2020-05-22 09:15:38 -0500

    Add files via upload

---
+++ Tastyfoodlib/FoodItems.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tastyfoodlib
+{
+   public class FoodItems
+    {
+        public FoodItems()
+        {
+            this.items = new List<string[]>();
+        }
+        public string[] Name { get; set; }
+        public double[] Price { get; set; }
+        public string[] Description { get; set; }
+        public int[] AmountInStock { get; set; }
+
+        public List<string[]> items { get; set; }
+    }
+}
